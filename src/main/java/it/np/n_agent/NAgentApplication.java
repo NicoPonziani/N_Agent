cf568379e07756daabd0bd4aa5c8bdce948477f0
Nico Ponziani <nico.ponziani@gmail.it>
2025-12-16 12:22:40 +0100

    init

---
+++ src/main/java/it/np/n_agent/NAgentApplication.java
@@ -0,0 +1,13 @@
+package it.np.n_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/np/n_agent/NAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package it.np.n_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
